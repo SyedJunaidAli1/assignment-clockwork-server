a0948a9cbd4afcfffdf13b2cebfed5a25051881c
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-01-18 19:34:47 +0530

    timestamps added in shop schema

--- src/models/shop.js
+++ src/models/shop.js
@@ -1,19 +1,24 @@
 import mongoose from "mongoose";
 
-const shopSchema = new mongoose.Schema({
-  shopDomain: {
-    type: String,
-    required: true,
-    unique: true,
+const shopSchema = new mongoose.Schema(
+  {
+    shopDomain: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    installedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    postPurchaseMessage: {
+      type: String,
+      default: "Thank you for your order!",
+    },
   },
-  installedAt: {
-    type: Date,
-    default: Date.now,
+  {
+    timestamps: true,
   },
-  postPurchaseMessage: {
-    type: String,
-    default: "Thank you for your order!",
-  },
-});
+);
 
 export default mongoose.model("Shop", shopSchema);
