c76b6292e36d293a2c8de940aa82750f3ceb5adf
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-01-18 16:12:01 +0530

    initial commit and mongodb connected

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./config/db.js";
+import app from "./app.js";
+
+const PORT = process.env.PORT;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Shopify App Server Running");
+});
